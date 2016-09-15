1fc78ac7ab4b0521f09733dbe58443216ea39d7d
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-09-16 00:51:17 +0900

    Add UpdateXcodeprojAction

--- lib/fastlane/plugin/update_xcodeproj/actions/update_xcodeproj_action.rb
+++ lib/fastlane/plugin/update_xcodeproj/actions/update_xcodeproj_action.rb
@@ -1,8 +1,26 @@
+# frozen_string_literal: true
 module Fastlane
   module Actions
     class UpdateXcodeprojAction < Action
       def self.run(params)
-        UI.message("The update_xcodeproj plugin is working!")
+        require 'xcodeproj'
+
+        options = params[:options]
+        project_path = params[:xcodeproj]
+        project = Xcodeproj::Project.open(project_path)
+
+        options.each do |key, value|
+          configs = project.objects.select { |obj| obj.isa == 'XCBuildConfiguration' && !obj.build_settings[key.to_s].nil? }
+          UI.user_error!("Info plist uses $(#{value}), but xcodeproj does not") unless configs.count.positive?
+
+          configs.each do |c|
+            c.build_settings[key.to_s] = value
+          end
+        end
+
+        project.save
+
+        UI.success("Updated #{params[:xcodeproj]} 💾.")
       end
 
       def self.description
@@ -15,11 +33,21 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "UPDATE_XCODEPROJ_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :xcodeproj,
+                                       env_name: "UPDATE_XCODEPROJ_XCODEPROJ",
+                                       description: "Path to your Xcode project",
+                                       optional: false,
+                                       default_value: DIR['*.xcodeproj'].first,
+                                       type: String,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("Please pass the path to the project, not the workspace") unless value.end_with?(".xcodeproj")
+                                         UI.user_error!("Could not find Xcode project") unless File.exist?(value)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :options,
+                                       env_name: "UPDATE_XCODEPROJ_OPTIONS",
+                                       description: "Key & Value pair that you will update xcode project",
+                                       optional: false,
+                                       type: Hash)
         ]
       end
 
@@ -27,7 +55,7 @@ module Fastlane
         # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
         # See: https://github.com/fastlane/fastlane/blob/master/fastlane/docs/Platforms.md
         #
-        # [:ios, :mac, :android].include?(platform)
+        [:ios, :mac].include?(platform)
         true
       end
     end
