dc5f02d9372446a32b83b1e58b84725da6d9715f
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2017-07-29 23:59:26 +0900

    Update is_supported?

--- lib/fastlane/plugin/update_xcodeproj/actions/update_xcodeproj_action.rb
+++ lib/fastlane/plugin/update_xcodeproj/actions/update_xcodeproj_action.rb
@@ -53,11 +53,7 @@ module Fastlane
       end
 
       def self.is_supported?(platform)
-        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
-        # See: https://github.com/fastlane/fastlane/blob/master/fastlane/docs/Platforms.md
-        #
         [:ios, :mac].include?(platform)
-        true
       end
     end
   end
