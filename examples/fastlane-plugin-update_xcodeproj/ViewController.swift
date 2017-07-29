d65608a574a8fcd113c8c2d2b5b939e31c8e25d8
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2017-07-30 01:35:21 +0900

    Add example xcode project

---
+++ examples/fastlane-plugin-update_xcodeproj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fastlane-plugin-update_xcodeproj
+//
+//  Created by Fumiya Nakamura on 2017/07/30.
+//  Copyright © 2017 Fumiya Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
